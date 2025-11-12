be56fe4c3a8e6df66523aa11d65366cfb1b0ded1
divine <guptadivine0611@gmail.com>
2025-11-13 02:42:26 +0530

    last commit

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express();
 app.use(express.json());
 const allowedOrigins = [
   "http://localhost:5173",
-  "https://spark-frontend.vercel.app"
+  "https://spark-frontend-olive.vercel.app/"
 ];
 
 app.use(cors({
